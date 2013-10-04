0539d37936fc67f7a5de962827f4d6b25f1f506f
Curtis Maloney <curtis@tinbrain.net>
2013-10-04 21:02:27 +1000

    Factor our common code

--- webdav_storage/backend.py
+++ webdav_storage/backend.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.core.files.storage import Storage
 
 import requests
+from requests.exceptions import RequestException
+
 from urlparse import urljoin
 
 try:
@@ -25,11 +27,14 @@ class WebDavStorage(Storage):
         """
         return urljoin(WEBDAV_PUBLIC if external else WEBDAV_ROOT, name)
 
+    def _request(self, method, name, **kwargs):
+        return self.session.request(method, self._build_url(name), **kwargs)
+
     def _open(self, name, mode='rb'):
         """
         Retrieves the specified file from storage.
         """
-        resp = self.session.get(self._build_url(name))
+        resp = self._request('GET', name)
         assert resp.status_code == 200
         return StringIO(resp.content)
 
@@ -39,20 +44,20 @@ class WebDavStorage(Storage):
         a proper File object or any python file-like object, ready to be read
         from the beginning.
         """
-        resp = self.session.put(self._build_url(name), content)
+        resp = self._request('PUT', name, data=content)
 
     def delete(self, name):
         """
         Deletes the specified file from the storage system.
         """
-        resp = self.session.delete(self._build_url(name))
+        resp = self._request('DELETE', name)
 
     def exists(self, name):
         """
         Returns True if a file referened by the given name already exists in the
         storage system, or False if the name is available for a new file.
         """
-        resp = self.session.head(self._build_url(name))
+        resp = self._request('HEAD', name)
         return resp.status_code != 404
 
     def listdir(self, path):
@@ -60,14 +65,14 @@ class WebDavStorage(Storage):
         Lists the contents of the specified path, returning a 2-tuple of lists;
         the first item being directories, the second item being files.
         """
-        resp = self.session.get(self._build_path(path))
+        resp = self._request('GET', path)
         # XXX?
 
     def size(self, name):
         """
         Returns the total size, in bytes, of the file specified by name.
         """
-        resp = self.session.head(self._build_url(name))
+        resp = self._request('HEAD', name)
         return int(resp['content-lenght'])
 
     def url(self, name):
@@ -80,19 +85,19 @@ class WebDavStorage(Storage):
     ### django-filebrowser extensions
 
     def isdir(self, name):
-        resp = self.session.head(self._build_url(name))
+        resp = self._request('HEAD', name)
         if resp.status_code == 404:
             return False
         # ?
 
     def isfile(self, name):
-        resp = self.session.head(self._build_url(name))
+        resp = self._request('HEAD', name)
         if resp.status_code == 404:
             return False
         # ?
 
     def move(self, old_file_name, new_file_name, allow_overwrite=False):
-        resp = self.session.move(self._build_url(old_file_name),
+        resp = self._request('MOVE', old_file_name,
             headers={
                 'Destination': self._build_url(new_file_name),
             }
